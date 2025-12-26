e166d109fac67f2e782c52d9efc6a593ab476471
cyl3el2cleal2 <cyl3el2cleal2@hotmail.com>
2025-12-26 14:33:29 +0700

    fix: cors

--- apps/backend/src/app.py
+++ apps/backend/src/app.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db import User, create_db_and_tables
 from src.schemas import UserCreate, UserRead, UserUpdate
@@ -25,6 +26,19 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 app.include_router(price_history_router, prefix="/price", tags=["price-history"])
